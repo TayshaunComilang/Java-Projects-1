78c795c9dcc82d731446155727c03ea582ef002a
TayshaunComilang <tmc3579@students.uc-bcf.edu.ph>
2024-09-13 09:50:24 +0800

    Add files via upload

---
+++ Asking the User Input Name.java
@@ -0,0 +1,31 @@
+//Comilang, Tayshaun M.
+//CITCS 1N-A
+//September 13, 2024
+
+package com.techvidvan.helloworldexample;
+import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		        
+		System.out.print("What is your name: ");
+		String name = scanner.nextLine();
+		        
+		System.out.print("How old are you: ");
+		int age = scanner.nextInt();
+		        
+		System.out.println("How are you?: ");
+		char grade = scanner.next().charAt(0);
+		        
+		System.out.println("What is your GPA last semester?:");
+		double gpa = scanner.nextDouble();
+		        
+		System.out.println("Are you a student (true/false):");
+		boolean block = scanner.nextBoolean();
+		   
+
+	}
+
+}
