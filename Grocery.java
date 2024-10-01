144f580f2c8c046fa7b0f8712806d084a0be41bd
TayshaunComilang <tmc3579@students.uc-bcf.edu.ph>
2024-10-01 12:16:52 +0800

    Update Grocery.java

--- Grocery.java
+++ Grocery.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Comilang, Tayshaun M.
+//CITCS 1N-A
+
 package grocery;
 import java.util.Scanner;
 public class Grocery {
