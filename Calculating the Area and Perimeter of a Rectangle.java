868f285396913b8f5cf960e8a811dcf3aed01174
TayshaunComilang <tmc3579@students.uc-bcf.edu.ph>
2024-10-01 12:50:12 +0800

    Update Calculating the Area and Perimeter of a Rectangle.java

--- Calculating the Area and Perimeter of a Rectangle.java
+++ Calculating the Area and Perimeter of a Rectangle.java
@@ -5,12 +5,17 @@
 public class Main {
     public static void main(String[] args) {
         
+        //Values of length and width
         double length = 19.5; 
         double width = 7.2;   
         
+        //Formula of the area of a rectangle
         double area = length * width;
+
+        //Formula to calculate the perimeter
         double perimeter = 2 * (length + width);
-        
+
+        //Display the result
         System.out.println("Length of the garden: " + length + " meters");
         System.out.println("Width of the garden: " + width + " meters");
         System.out.println("Area of the garden: " + area + " square meters");
