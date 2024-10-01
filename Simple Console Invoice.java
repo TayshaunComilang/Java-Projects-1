66653d023621f219b0dcd41ae232fb6cc444e135
TayshaunComilang <tmc3579@students.uc-bcf.edu.ph>
2024-10-01 12:52:36 +0800

    Update Simple Console Invoice.java

--- Simple Console Invoice.java
+++ Simple Console Invoice.java
@@ -2,15 +2,23 @@
 
 public class Main {
   public static void main(String[] args) {
+
+    //Store Information
     System.out.println("Tayshaun's Wardrobe");
     System.out.println("Antamok Trail, Tuding,\nItogon, Benguet, 2604");
+
+    //Itemized List
     System.out.println("\nItems\t\tQuantities\t\tPrice");
     System.out.println("Baggy Pants\t\t1\t\t\t1,000");
     System.out.println("Croptop\t\t\t1\t\t\t200");
     System.out.println("Short\t\t\t1\t\t\t100");
     System.out.println("Rashguard\t\t1\t\t\t300");
     System.out.println("Bag\t\t\t\t1\t\t\t500");
+
+    //Total amount
     System.out.println("\nTotal:\t\t\t\t\t\t2,100");
+
+    //Additional Notes
     System.out.println("\nThank you for shopping at Tayshaun's Wardrobe! If you have any issues, don't hesitate to contact me at 09123467890");
   }
 
