c5abd60efea97cdf74d0ac5514a8c5ca6d70085b
TayshaunComilang <tmc3579@students.uc-bcf.edu.ph>
2024-10-01 12:16:03 +0800

    Update Grocery Shopping Calculator.java

--- GroceryCal.java
+++ GroceryCal.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package grocerycal;
+//Comilang, Tayshaun M.
+//CITCS 1N-A
+//September 13, 2024
 
+package grocerycal;
 import java.util.Scanner;
 public class GroceryCal{
 
